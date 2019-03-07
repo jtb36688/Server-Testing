b96e38d1f4bf584a74d2cfeaf6d889674045818d
Jacob Bryan <jtb36688@gmail.com>
2019-03-07 18:42:01 -0500

    Separated routes into different describe() functions

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,22 +6,45 @@ describe('server.js', () => {
     it('should be associated with the testing object in knexfile', () => {
         expect(process.env.DB_ENV).toBe('testing')
     })
+})
 
+describe('GET /', () => {
     it('should return a status code of 200 on GET', async () => {
         const res = await request(server).get('/')
         expect(res.status).toBe(200)
     })
+    it('should return JSON', async () => {
+        const res = await request(server).get('/')
+        expect(res.type).toBe('application/json')
+    })
+})
 
+
+describe('POST /', () => {
     it('should return a status code of 200 on POST', async () => {
         const res = await request(server).post('/')
         expect(res.status).toBe(201)
     })
+    it('should return JSON', async () => {
+        const res = await request(server).post('/')
+        expect(res.type).toBe('application/json')
+    })
+})
 
+
+describe('PUT /', () => {
     it('should return a status code of 200 on PUT', async () => {
         const res = await request(server).put('/')
         expect(res.status).toBe(201)
     })
+    it('should return JSON', async () => {
+        const res = await request(server).put('/')
+        expect(res.type).toBe('application/json')
+    })
+})
 
+
+describe('DELETE /', () => {
     it('should return a status code of 200 on DELETE', async () => {
         const res = await request(server).delete('/')
         expect(res.status).toBe(204)
