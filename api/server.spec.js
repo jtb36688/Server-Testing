141b39ff404ff97bf091f650c8aec34cac3afcdd
Jacob Bryan <jtb36688@gmail.com>
2019-03-07 18:38:23 -0500

    Added tests for each CRUD method

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,11 +6,24 @@ describe('server.js', () => {
     it('should be associated with the testing object in knexfile', () => {
         expect(process.env.DB_ENV).toBe('testing')
     })
-})
 
-describe('GET /', () => {
     it('should return a status code of 200 on GET', async () => {
         const res = await request(server).get('/')
         expect(res.status).toBe(200)
-    }
+    })
+
+    it('should return a status code of 200 on POST', async () => {
+        const res = await request(server).post('/')
+        expect(res.status).toBe(201)
+    })
+
+    it('should return a status code of 200 on PUT', async () => {
+        const res = await request(server).put('/')
+        expect(res.status).toBe(201)
+    })
+
+    it('should return a status code of 200 on DELETE', async () => {
+        const res = await request(server).delete('/')
+        expect(res.status).toBe(204)
+    })
 })
